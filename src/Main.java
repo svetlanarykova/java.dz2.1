4743e12017bc60d0a0af06c8266d4c32fae6c112
Светлана Рыкова <srv8@mail.ru>
2022-06-22 18:31:07 +0300

    Added src/main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 600;
+        int mile = 20;
+        int bonusMile = ticketPrice / mile;
+        System.out.println("Бонусных миль: " + bonusMile);
+    }
+}
